ff5d26e17f6417c2d48c0b5cd4f5b0793d914ddd
Vladyslav Luhovatskyi <vladyslav.luhovatskyi@gmail.com>
2024-02-22 20:12:36 +0000

    Solution for Task1 - Anagram

---
+++ Anagram.UI/UserUI.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Test1_Anagram;
+
+namespace Test1_Anagram
+
+{
+    internal class UserUI : Anagram
+    {
+        public void Run()
+        {
+            Console.WriteLine("Well Come to Program");
+
+            var anagram = new Anagram();
+            string userInput;
+
+            do
+            {
+                Console.WriteLine("Please Enter a Word: ");
+                userInput = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(userInput))
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid word. ");
+                }
+
+            } while (string.IsNullOrEmpty(userInput));
+
+            Console.WriteLine("Reversed Word: " + anagram.ReversedWord(userInput));
+        }
+        
+        public static void Main(string[] args)
+        {
+            var program = new UserUI();
+            program.Run();
+        }
+    }
+}
