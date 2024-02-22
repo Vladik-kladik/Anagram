9e693ac12a0dbd712cd8f61f4a192940cbe96a5d
Vladik-kladik <vladyslav.luhovatskyi@gmail.com>
2024-02-22 21:07:26 +0100

    переназва тест за назвою класу

--- Anagram.Test/Anagram.Test.cs
+++ Anagram.Test/Anagram.Test.cs
@@ -11,8 +11,13 @@ namespace Unit_Test_Anagram;
         public void Revers_ValidWord_returnsRevers()
         {
             Anagram anagram = new Anagram();
+<<<<<<< HEAD
             string reversedSentence = anagram.Reverse("a1bcd efg!h");
             Assert.That(reversedSentence, Is.EqualTo("d1cba hgf!e"));
+=======
+            string reversedSentence = anagram.ReversedWord("Hello World!");
+            Assert.That(reversedSentence, Is.EqualTo("!dlroW olleH"));
+>>>>>>> 5b70903 (переназва тест за назвою класу)
         }
         
       
@@ -20,6 +25,7 @@ namespace Unit_Test_Anagram;
         public void ReverseWord_WithNonAlnon_alphabetic_characters()
         {
             Anagram anagram = new Anagram();
+<<<<<<< HEAD
             string reversedSentence = new Anagram().Reverse("  a1bcd    efg!h");
             Assert.That(reversedSentence, Is.EqualTo("  d1cba    hgf!e"));
         }
@@ -36,6 +42,26 @@ namespace Unit_Test_Anagram;
             Assert.AreEqual(string.Empty, resultNull);
             Assert.AreEqual(string.Empty, resultEmpty);
            
+=======
+            string reversedSentence = new Anagram().ReversedWord("123abc!412");
+            Assert.That(reversedSentence, Is.EqualTo("214!cba321"));
+        }
+        
+        [Test]
+        public void ReverseWord_NullWord_ReturnsNull()
+        {
+           var anagram = new Anagram();
+           string reversedSentence = anagram.ReversedWord(null);
+           Assert.Null(reversedSentence);
+        }
+        
+        [Test]
+        public void Reverse_NullWord_ReturnsNull()
+        {
+            var anagram = new Anagram();
+            string reversedSentence = anagram.Reverse(null);
+            Assert.Null(reversedSentence);
+>>>>>>> 5b70903 (переназва тест за назвою класу)
         }
       
     }
