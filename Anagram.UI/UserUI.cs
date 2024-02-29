8677ff16837abb5322b6c01b84a9050ea9bee317
Vladik-kladik <vladyslav.luhovatskyi@gmail.com>
2024-02-29 22:23:55 +0100

    правки за 29.02
    1. ReversedWord був ініціалізований як приватний метод у revers методі
    2. Було перероблено unitTest, де слова зберігають порядок, а "не символи" залишаються на своїх місцях
    3. Також переписав метод Reverse, де замінив foreach метод на for

--- Anagram.UI/UserUI.cs
+++ Anagram.UI/UserUI.cs
@@ -2,16 +2,17 @@
 using Test1_Anagram;
 
 namespace Test1_Anagram
+
 {
     internal class UserUI : Anagram
     {
         public void Run()
         {
             Console.WriteLine("Well Come to Program");
-        
+
             var anagram = new Anagram();
-            var userInput = String.Empty;
-            
+            string userInput;
+
             do
             {
                 Console.WriteLine("Please Enter a Word: ");
@@ -24,11 +25,7 @@ namespace Test1_Anagram
 
             } while (string.IsNullOrEmpty(userInput));
 
-<<<<<<< HEAD
-            Console.WriteLine("Reversed Word: " + anagram.Reverse(userInput));
-=======
             Console.WriteLine("Reversed Word: " + anagram.ReversedWord(userInput));
->>>>>>> origin/main
         }
         
         public static void Main(string[] args)
