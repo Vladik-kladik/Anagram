78420dacb7cb2f28ffe905ff7bfe6fac0eeed6a7
Vladik-kladik <vladyslav.luhovatskyi@gmail.com>
2024-03-01 23:19:41 +0100

    Допрацювання методів ReversedWord
    1. Переписав методи "По-людськи", для отримання правильного результату, коли символи не-літери мають лишатись на місцях
    2. Також трішки переписав UnitTest, так щоб вони співпадали із прикладами в умові задачі на студенстькому силабусі.

--- Anagram.Test/Anagram.Test.cs
+++ Anagram.Test/Anagram.Test.cs
@@ -11,13 +11,8 @@ namespace Unit_Test_Anagram;
         public void Revers_ValidWord_returnsRevers()
         {
             Anagram anagram = new Anagram();
-<<<<<<< HEAD
             string reversedSentence = anagram.Reverse("Hello World!");
             Assert.That(reversedSentence, Is.EqualTo("olleH !dlroW"));
-=======
-            string reversedSentence = anagram.ReversedWord("Hello World!");
-            Assert.That(reversedSentence, Is.EqualTo("!dlroW olleH"));
->>>>>>> origin/main
         }
         
       
@@ -25,11 +20,7 @@ namespace Unit_Test_Anagram;
         public void ReverseWord_WithNonAlnon_alphabetic_characters()
         {
             Anagram anagram = new Anagram();
-<<<<<<< HEAD
             string reversedSentence = new Anagram().Reverse("123abc!412");
-=======
-            string reversedSentence = new Anagram().ReversedWord("123abc!412");
->>>>>>> origin/main
             Assert.That(reversedSentence, Is.EqualTo("214!cba321"));
         }
         
@@ -37,11 +28,7 @@ namespace Unit_Test_Anagram;
         public void ReverseWord_NullWord_ReturnsNull()
         {
            var anagram = new Anagram();
-<<<<<<< HEAD
            string reversedSentence = anagram.Reverse(null);
-=======
-           string reversedSentence = anagram.ReversedWord(null);
->>>>>>> origin/main
            Assert.Null(reversedSentence);
         }
         
